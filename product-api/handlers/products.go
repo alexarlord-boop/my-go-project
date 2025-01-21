4828e1295d3ec671de291cfb9e83c1eed826e5dc
alexarlord-boop <rezerw0.1@yandex.ru>
2025-01-21 12:10:01 +0200

    Add product validation using Go Validator and implement SKU format check

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"my-go-project/product-api/data"
 	"net/http"
@@ -71,11 +72,15 @@ func (p *Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 			return
 		}
 
-		// err = product.Validate()
-		// if err != nil {
-		// 	http.Error(w, "Product validation failed", http.StatusBadRequest)
-		// 	return
-		// }
+		err = product.Validate()
+		if err != nil {
+			http.Error(
+				w,
+				fmt.Sprintf("Error validating product: %s", err),
+				http.StatusBadRequest,
+			)
+			return
+		}
 
 		ctx := context.WithValue(r.Context(), KeyProduct{}, product)
 		req := r.WithContext(ctx)
